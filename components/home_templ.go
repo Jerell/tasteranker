4198af2917489c00d697f26d9d79ca8e33452c83
Jerell James <jerell1998@gmail.com>
2024-10-30 21:13:14 +0000

    comparison head to head

--- components/home_templ.go
+++ components/home_templ.go
@@ -30,6 +30,14 @@ func Home() templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
+		templ_7745c5c3_Err = Comparison().Render(ctx, templ_7745c5c3_Buffer)
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		templ_7745c5c3_Err = templ.WriteWatchModeString(templ_7745c5c3_Buffer, 2)
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
 		return templ_7745c5c3_Err
 	})
 }
