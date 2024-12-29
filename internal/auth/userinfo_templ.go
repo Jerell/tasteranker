6cc3e3b0ab9218f794ba7693c7d39951b30a138b
Jerell James <jerell1998@gmail.com>
2024-12-29 22:51:02 +0000

    styling and theme

--- internal/auth/userinfo_templ.go
+++ internal/auth/userinfo_templ.go
@@ -33,7 +33,7 @@ func UserInfo(name string) templ.Component {
 		var templ_7745c5c3_Var2 string
 		templ_7745c5c3_Var2, templ_7745c5c3_Err = templ.JoinStringErrs(name)
 		if templ_7745c5c3_Err != nil {
-			return templ.Error{Err: templ_7745c5c3_Err, FileName: `internal/auth/userinfo.templ`, Line: 5, Col: 31}
+			return templ.Error{Err: templ_7745c5c3_Err, FileName: `internal/auth/userinfo.templ`, Line: 5, Col: 24}
 		}
 		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var2))
 		if templ_7745c5c3_Err != nil {
